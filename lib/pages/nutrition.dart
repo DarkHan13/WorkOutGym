36890355b6036aa0907f2f33b32159fc238e04e1
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2023-05-19 02:29:58 +0600

    nutrition ui update

--- lib/pages/nutrition.dart
+++ lib/pages/nutrition.dart
@@ -24,9 +24,7 @@ class UserNutrition extends StatelessWidget{
           body: TabBarView(
             children: [
               Drying(),
-              const Center(
-                child: Text('Масса'),
-              ),
+              Drying(),
             ],
           ),
         )
@@ -63,32 +61,66 @@ class _DryingState extends State<Drying> {
           color: Colors.white,
         ),
         const Text('Выберите меню'),
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image(
-              image: NetworkImage('https://www.eatingwell.com/thmb/m5xUzIOmhWSoXZnY-oZcO9SdArQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/article_291139_the-top-10-healthiest-foods-for-kids_-02-4b745e57928c4786a61b47d8ba920058.jpg'),
-              height: 100,
-            ),
-            Image(
-              image: NetworkImage('https://www.eatingwell.com/thmb/m5xUzIOmhWSoXZnY-oZcO9SdArQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/article_291139_the-top-10-healthiest-foods-for-kids_-02-4b745e57928c4786a61b47d8ba920058.jpg'),
-              height: 100,
-            ),
-          ],
+        DryingMenu(),
+      ],
+    );
+  }
+
+}
+
+class DryingMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      childAspectRatio: 1.3,
+      crossAxisCount: 2,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(5.0),
+          child: ListView(
+            children: const <Widget>[
+              Image(
+                image: NetworkImage('https://pbs.twimg.com/media/DP-NvcCW4AAMWw4?format=jpg&name=medium'),
+              ),
+              Text('Завтрак'),
+            ],
+          ),
         ),
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image(
-              image: NetworkImage('https://www.eatingwell.com/thmb/m5xUzIOmhWSoXZnY-oZcO9SdArQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/article_291139_the-top-10-healthiest-foods-for-kids_-02-4b745e57928c4786a61b47d8ba920058.jpg'),
-              height: 100,
-            ),
-            Image(
-              image: NetworkImage('https://www.eatingwell.com/thmb/m5xUzIOmhWSoXZnY-oZcO9SdArQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/article_291139_the-top-10-healthiest-foods-for-kids_-02-4b745e57928c4786a61b47d8ba920058.jpg'),
-              height: 100,
-            ),
-          ],
+        Container(
+          margin: const EdgeInsets.all(5.0),
+          child: ListView(
+            children: const <Widget>[
+              Image(
+                image: NetworkImage('https://i.ytimg.com/vi/ttyE6LNnNFc/maxresdefault.jpg'),
+              ),
+              Text('Перекус'),
+            ],
+          ),
         ),
+        Container(
+          margin: const EdgeInsets.all(5.0),
+          child: ListView(
+            children: const <Widget>[
+              Image(
+                image: NetworkImage('https://levgon.ru/wp-content/uploads/2014/02/%D0%B3%D1%80%D1%83%D0%B4%D0%BA%D0%B0-%D1%81-%D1%80%D0%B8%D1%81%D0%BE%D0%BC.jpg'),
+              ),
+              Text('Обед'),
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(5.0),
+          child: ListView(
+            children: const <Widget>[
+              Image(
+                image: NetworkImage('https://ggym.ru/wp-content/uploads/2018/12/egg.jpg'),
+              ),
+              Text('Ужин')
+            ],
+          )
+        )
       ],
     );
   }
